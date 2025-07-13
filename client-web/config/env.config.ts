8ae03f8d5d8896007186e32263443e01ba9d7b17
Meowveloper <meowveloper.dev@gmail.com>
2025-07-13 13:13:48 +0700

    fix env error

--- client-web/config/env.config.ts
+++ client-web/config/env.config.ts
@@ -1,7 +1,20 @@
 type ENV_KEYS_TYPE = "NEXT_PUBLIC_API_URL";
 
 export const get_env = (key: ENV_KEYS_TYPE): string => {
-	const value = process.env[key];
-	if (!value) throw new Error("the evnironment variable" + key + " is not defined");
+	console.log("key", key);
+	let value: string | undefined;
+
+	switch (key) {
+		case "NEXT_PUBLIC_API_URL":
+			value = process.env.NEXT_PUBLIC_API_URL;
+			break;
+		default:
+			value = undefined;
+	}
+
+	console.log("value", value);
+	console.log("manual 1", process.env["NEXT_PUBLIC_API_URL"]);
+	console.log("manual 2", process.env.NEXT_PUBLIC_API_URL);
+	if (!value) throw new Error("the environment variable " + key + " is not defined");
 	return value;
 };
