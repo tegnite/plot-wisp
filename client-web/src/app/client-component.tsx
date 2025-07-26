6e315df65813935c54df74a0f547dc6110820655
Meowveloper <meowveloper.dev@gmail.com>
2025-07-26 07:11:15 +0700

    fix dev container

--- client-web/src/app/client-component.tsx
+++ client-web/src/app/client-component.tsx
@@ -1,6 +1,18 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export default function Client_Component() {
-	const text = process.env.NEXT_PUBLIC_TEXT;
-	return <div>client: {text}</div>;
+	const [message, set_message] = useState<string>("");
+
+	useEffect(() => {
+		const url = process.env.NEXT_PUBLIC_API_URL;
+		const get_message = async () => {
+			const response = await fetch(url as string);
+			const data = await response.json();
+			set_message(data.message as string);
+		};
+		get_message();
+	}, []);
+	return <div>message from server(client component): {message}</div>;
 }
--- client-web/src/app/page.tsx
+++ client-web/src/app/page.tsx
@@ -1,12 +1,15 @@
 import Client_Component from "./client-component";
 
-export default function Home() {
-	const text = process.env.TEXT;
+export default async function Home() {
+	const url = process.env.API_URL;
+	const response = await fetch(url as string);
+	const data = await response.json();
+	console.log("data", data);
 	return (
 		<div className="w-screen h-screen flex flex-col gap-3 justify-center items-center">
-			<div>server: {text}</div>
+			<div>message from server (server component): {data.message}</div>
 			<Client_Component />
-			<div>Testing changes</div>
+			<div>Testing changes again!!</div>
 		</div>
 	);
 }
