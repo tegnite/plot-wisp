c2610c63efbd626530974133c4e64b3f40d833f8
MinTheinKyaw <mintheinky@wconsole@gmail.com>
2025-09-01 17:10:31 +0900

    MetaData_update

--- client-web/src/app/layout.tsx
+++ client-web/src/app/layout.tsx
@@ -6,6 +6,55 @@ import { Mode_Toggle } from "@/components/general/Mode-Toggle";
 export const metadata: Metadata = {
 	title: "Plot Wisp",
 	description: "story website",
+	keywords: "plot, wisp, story, website",
+	publisher: "Plot Wisp",
+	authors: [{ name: "Plot Wisp" }],
+	creator: "Plot Wisp",
+	openGraph: {
+		title: "Plot Wisp",
+		description: "story website",
+		url: "https://plotwisp.com",
+		siteName: "Plot Wisp",
+		images: [
+			{
+				url: "",
+				width: 1200,
+				height: 630,
+				alt: "Plot Wisp",
+			},
+		],
+		type: "website",
+		locale: "en_US",
+	},
+	twitter: {
+		title: "Plot Wisp",
+		description: "story website",
+		card: "summary_large_image",
+		site: "@plotwisp",
+		creator: "@plotwisp",
+		images: [
+			{
+				url: "",
+				width: 1200,
+				height: 630,
+				alt: "Plot Wisp",
+			},
+		],
+	},
+	icons: {
+		icon: "/favicon.ico",
+		shortcut: "/favicon.ico",
+		apple: "/apple-touch-icon.png",
+	},
+	facebook: {
+		appId: "", // Replace with your actual Facebook App ID
+	},
+	verification: {
+		google: "google-site-verification-code", // Replace with your actual Google verification code
+		yandex: "yandex-site-verification-code", // Replace with your actual Yandex verification code
+	},
+	
+	
 };
 
 export default function RootLayout({
