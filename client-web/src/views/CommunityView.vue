9460dfd58b9b0f33477e0aee0588dee5c4314fbd
meowveloper <meowveloper.dev@gmail.com>
2025-09-24 22:08:38 +0700

    test/quill

--- client-web/src/views/CommunityView.vue
+++ client-web/src/views/CommunityView.vue
@@ -1,5 +1,30 @@
 <template>
     <div>
         <h1>Community page</h1>
+
+      <div class="w-full h-56">
+          <div ref="editor_container" class="bg-red-500 w-full h-full"></div>
+      </div>
     </div>
 </template>
+<script setup lang="ts">
+import Quill from 'quill';
+import { onMounted, ref, type Ref } from 'vue';
+
+
+// refs
+const editor_container : Ref<HTMLElement | null> = ref(null);
+
+onMounted(() => {
+  if(editor_container.value) {
+    const options = {
+      modules: {
+        toolbar: true,
+      },
+      placeholder: 'Compose an epic...',
+      theme: 'snow'
+    };
+    const quill = new Quill(editor_container.value, options);
+  }
+})
+</script>
